c46f4ac45ebaed5b56d91808b1f38fc300f0e253
anvilnext <chereva@ukr.net>
2018-11-05 19:59:43 +0200

    new tests

---
+++ SUPER_MEGA_COOL_EXTREME_ULTIMATE_PROJECTTests/Controllers/NewControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+
+namespace SUPER_MEGA_COOL_EXTREME_ULTIMATE_PROJECTTests.Controllers
+{
+    [TestClass()]
+    class NewControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            if (Assert.Equals(Config.Start(), null))
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void AboutTest()
+        {
+            Mock<config.Config>  = new Mock<config.Config>();
+            Assert.AreEqual(config.getInstance(), null);
+        }
+    }
+}
